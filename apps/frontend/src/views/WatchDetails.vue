0f7bc0560a9f26bcb6bc4e25e3a2271d53b331a6
Stefan Schärmeli <info@schaermu.ch>
2024-04-29 10:12:52 +0200

    feat: adds crude list view with delete/edit link

---
+++ apps/frontend/src/views/WatchDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Edit watch {{ id }}</h1>
+        <pre>
+            {{ watch }}
+        </pre>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import WatchesApi from '@/services/watches'
+
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    }
+})
+
+const watch = ref(null)
+watch.value = await WatchesApi.fetchById(props.id)
+</script>
--- apps/frontend/src/views/WatchesView.vue
+++ apps/frontend/src/views/WatchesView.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="w-6/12 mx-auto mt-10">
-        <h1>Create new watch</h1>
-        <Form @submit="onSubmit" :validation-schema="schema">
-            <div class="flex gap-2">
-                <TwInput class="flex-auto" name="url" label="URL" :inline="true" inputClass="input-lg"
-                    labelClass="input-lg" />
-                <button type="submit" class="btn btn-lg btn-primary">
-                    <span v-if="isSubmitting" class="loading loading-spinner"></span>
-                    Next
-                </button>
+    <div class="flex flex-col">
+        <div class="w-6/12 mx-auto mt-10">
+            <h1>Create new watch</h1>
+            <Form @submit="onSubmit" :validation-schema="schema">
+                <div class="flex gap-2">
+                    <TwInput class="flex-auto" name="url" label="URL" :inline="true" inputClass="input-lg"
+                        labelClass="input-lg" />
+                    <button type="submit" class="btn btn-lg btn-primary">
+                        <span v-if="isSubmitting" class="loading loading-spinner"></span>
+                        Next
+                    </button>
+                </div>
+            </Form>
+        </div>
+        <div class="w-9/12 mx-auto mt-10">
+            <div class="overflow-x-auto">
+                <table class="table table-zebra">
+                    <thead>
+                        <tr>
+                            <th>Name/URL</th>
+                            <th>Last check</th>
+                            <th>Last change</th>
+                            <th>Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="watch in watches" :key="watch.id">
+                            <td>{{ watch.url }}</td>
+                            <td>{{ watch.last_check }}</td>
+                            <td>{{ watch.last_change }}</td>
+                            <td>{{ watch.status }}</td>
+                            <td>
+                                <router-link :to="`/watches/${watch.id}`"
+                                    class="btn btn-sm btn-primary">Details</router-link>
+                                <button class="btn btn-sm btn-error"
+                                    @click.prevent="promptDelete(watch.id)">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        </Form>
+        </div>
     </div>
 </template>
 
@@ -19,6 +50,7 @@
 import { ref } from 'vue'
 import { Form } from 'vee-validate'
 import * as yup from 'yup'
+import { useRouter } from 'vue-router'
 
 import TwInput from '@/components/forms/TwInput.vue'
 import WatchesApi from '@/services/watches'
@@ -27,18 +59,35 @@ const schema = yup.object({
     url: yup.string().required().url(),
 })
 
+const watches = ref([])
 const isSubmitting = ref(false)
+const router = useRouter()
+
+const watchResult = await WatchesApi.fetchAll()
+if (watchResult.count > 0) {
+    watches.value = watchResult.results
+}
 
 function onSubmit(values, actions) {
     isSubmitting.value = true
     WatchesApi.create(values)
-        .then(() => {
+        .then((data) => {
             actions.resetForm()
             isSubmitting.value = false
+            router.push(`/watches/${data.id}`)
         })
         .catch(() => {
             isSubmitting.value = false
         })
+}
+
+function promptDelete(id) {
+    if (confirm('Are you sure you want to delete this watch?')) {
+        WatchesApi.delete(id)
+            .then(() => {
+                watches.value = watches.value.filter((watch) => watch.id !== id)
+            })
+    }
 }
 
 </script>
