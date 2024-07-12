a46eb459c2ea8af338987e39783d20ee86274b1b
Stefan Schärmeli <519494+schaermu@users.noreply.github.com>
2024-07-12 09:02:24 +0200

    fix: align configs with blueprint

--- apps/frontend/src/components/forms/Input.cy.ts
+++ apps/frontend/src/components/forms/Input.cy.ts
@@ -3,9 +3,9 @@ import Input from './Input.vue';
 describe('Input', () => {
   it('should mount', () => {
     cy.mount(Input, {
-        props: {
-            name: 'foobar'
-        }
+      props: {
+        name: 'foobar'
+      }
     });
 
     cy.get('input').should('exist').and('have.attr', 'name', 'foobar');
